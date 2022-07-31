5a3fa10876284b29987e412e92229de321abef9f
mortalcoder25 <45671265+mortalcoder25@users.noreply.github.com>
2022-07-31 17:18:18 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,69 @@
+import express from "express";
+import cors from "cors";
+import { Sudoku } from "./Sudoku.js";
+import { Util } from "./Util.js";
+import dotenv from 'dotenv'
+
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+
+let board = [
+  [3, 0, 6, 5, 0, 8, 4, 0, 0],
+  [5, 2, 0, 0, 0, 0, 0, 0, 0],
+  [0, 8, 7, 0, 0, 0, 0, 3, 1],
+  [0, 0, 3, 0, 1, 0, 0, 8, 0],
+  [9, 0, 0, 8, 6, 3, 0, 0, 5],
+  [0, 5, 0, 0, 9, 0, 6, 0, 0],
+  [1, 3, 0, 0, 0, 0, 2, 5, 0],
+  [0, 0, 0, 0, 0, 0, 0, 7, 4],
+  [0, 0, 5, 2, 0, 6, 3, 0, 0],
+];
+
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false }))
+
+app.listen(port, () => console.log(`Server started on Port ${port}`))
+
+// Get request to generate a puzzle
+app.get("/puzzle", (req, res) => {
+  let sudoku = new Sudoku();
+  let puzzle = sudoku.puzzle;
+  res.status(200).send({ game: puzzle });
+});
+
+
+// Post request to solve a puzzle
+app.post("/solve", (req, res) => {
+  let puzzle = [];
+  Util.copyGrid(req.body.board, puzzle);
+  let sudoku = new Sudoku(puzzle);
+  let solution = sudoku.isSolvable();
+  let solvedSudoku;
+  let status;
+  if (solution) {
+    solvedSudoku = sudoku.solvedPuzzle;
+    status = true;
+  } else {
+    solvedSudoku = req.body.board;
+    status = false;
+  }
+  res.status(200).send({ solution: solvedSudoku, status: status });
+});
+
+
+// Post request to validate a puzzle
+app.post("/validate", (req, res) => {
+  let puzzle = [];
+  Util.copyGrid(req.body.board, puzzle);
+  let sudoku = new Sudoku(puzzle);
+  let status = sudoku.validate();
+  res.status(200).send({ status: status });
+});
