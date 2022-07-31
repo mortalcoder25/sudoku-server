bcb210758f8828f9cf9693af7c90cdd60b9d6feb
mortalcoder25 <45671265+mortalcoder25@users.noreply.github.com>
2022-07-31 20:29:53 +0530

    change in server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ let board = [
 ];
 
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000", "https://sudoku-client.vercel.app/"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }))
